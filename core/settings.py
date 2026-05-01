2a16680055fad94cad67c14d65f10c7c7969ef31
chakri31383 <144146114+chakri31383@users.noreply.github.com>
2026-05-02 03:14:58 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -79,14 +79,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
+        default=os.getenv("postgresql://postgres:amfdLSMkyNJcddBYVOCefoavWYHYuGeZ@postgres.railway.internal:5432/railway"),
+        conn_max_age=600,
+        ssl_require=False
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
